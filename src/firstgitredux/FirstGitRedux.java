68101463d964a928c5ae4b32ca79313e4bb79250
SKaufman7 <SKaufman7@L16408039532.wctc.msft>
2013-08-27 16:06:43 -0500

    First commit

---
+++ src/firstgitredux/FirstGitRedux.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgitredux;
+
+/**
+ *
+ * @author SKaufman7
+ */
+public class FirstGitRedux {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
