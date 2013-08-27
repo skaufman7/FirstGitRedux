a599b32c2d45dc1868ee07da844c2cfb71b3756f
SKaufman7 <SKaufman7@L16408039532.wctc.msft>
2013-08-27 16:08:54 -0500

    Added one line to main method

--- src/firstgitredux/FirstGitRedux.java
+++ src/firstgitredux/FirstGitRedux.java
@@ -14,6 +14,9 @@ public class FirstGitRedux {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Ello");
+        
+        
     }
 }
